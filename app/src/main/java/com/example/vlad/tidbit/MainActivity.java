819bd439a331cc51f8ad4ba5eef9b85940741b70
vfranco2 <vfranco2@oakland.edu>
2018-11-04 21:48:11 -0500

    added recycleview, cardview, made home scrollable

--- app/src/main/java/com/example/vlad/tidbit/MainActivity.java
+++ app/src/main/java/com/example/vlad/tidbit/MainActivity.java
@@ -6,11 +6,17 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements RecycleViewAdapter.ItemClickListener{
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -19,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
 
+    RecycleViewAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
         mDrawerLayout = findViewById(R.id.navmenubar);
 
-        //Things that happen when you tap stuff
+        //Things that happen when you tap menu stuff
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
@@ -37,13 +45,9 @@ public class MainActivity extends AppCompatActivity {
                         // close drawer when item is tapped
                         mDrawerLayout.closeDrawers();
 
-                        // Add code here to update the UI based on the item selected
-                        // For example, swap UI fragments here
-
                         return true;
                     }
                 });
-
 
         //Toolbar stuff
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -52,8 +56,30 @@ public class MainActivity extends AppCompatActivity {
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
 
+        //Recycle view stuff
+        ArrayList<String> articleHold = new ArrayList<>();
+        articleHold.add("Art");
+        articleHold.add("Astronomy");
+        articleHold.add("Auto");
+        articleHold.add("Fashion");
+        articleHold.add("Film");
+        articleHold.add("Finance");
+        articleHold.add("Music");
+        articleHold.add("Photography");
+        articleHold.add("Science");
+        articleHold.add("Sports");
+        articleHold.add("Tech");
+        articleHold.add("Word of the Day");
+        articleHold.add("World");
+
+        RecyclerView recyclerView = findViewById(R.id.cardRecycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new RecycleViewAdapter(this, articleHold);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
     }
 
+    //Handle drawer clicks
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -64,12 +90,10 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    //Handle recycler clicks
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+    }
 
-
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    //public native String stringFromJNI();
 }
