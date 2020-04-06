e01ed13986d57b6aff592066016efae465ed1bfc
Vlad Franco <vladfranco2@gmail.com>
2020-04-05 20:46:26 -0400

    Styles, AppShell, Nav, Dark Mode

--- TidBit.Android/MainActivity.cs
+++ TidBit.Android/MainActivity.cs
@@ -6,6 +6,11 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Xamarin.Forms;
+using Android.Support.V7.App;
+using static TidBit.App;
+using Android.Content.Res;
+using TidBit.Styles;
 
 namespace TidBit.Droid
 {
@@ -21,13 +26,54 @@ namespace TidBit.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
             LoadApplication(new App());
+            SetAppTheme();
         }
+
+        private void OnModeChanged(Page arg1, Theme theme)
+        {
+            if (theme == App.Theme.Light)
+            {
+                Delegate.SetLocalNightMode(AppCompatDelegate.ModeNightNo);
+            }
+            else
+            {
+                Delegate.SetLocalNightMode(AppCompatDelegate.ModeNightYes);
+            }
+            SetTheme(theme);
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        void SetAppTheme()
+        {
+            if (Resources.Configuration.UiMode.HasFlag(UiMode.NightYes))
+                SetTheme(App.Theme.Dark);
+            else
+                SetTheme(App.Theme.Light);
+        }
+
+        void SetTheme(Theme mode)
+        {
+            if (mode == App.Theme.Dark)
+            {
+                if (App.AppTheme == App.Theme.Dark)
+                    return;
+                App.Current.Resources = new DarkTheme();
+            }
+            else
+            {
+                if (App.AppTheme != App.Theme.Dark)
+                    return;
+                App.Current.Resources = new LightTheme();
+            }
+            App.AppTheme = mode;
         }
     }
 }
---
+++ TidBit/AppShell.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace TidBit
+{
+    public partial class AppShell : Shell
+    {
+        protected override bool OnBackButtonPressed()
+        {
+            Current.Navigation.PopAsync(false); return true;
+        }
+
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
