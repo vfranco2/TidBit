02e7aa7f1a31a879cda7796227dbde33e28a82e6
vfranco2 <vladfranco2@gmail.com>
2020-04-20 10:48:20 -0400

    Share Functionality

--- Xamarin.Forms_App/TidBit/Views/ArticleView.xaml.cs
+++ Xamarin.Forms_App/TidBit/Views/ArticleView.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Threading.Tasks;
 using TidBit.Models;
 using TidBit.ViewModels;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace TidBit.Views
@@ -26,9 +28,25 @@ namespace TidBit.Views
             webView.Reload();
         }
 
+        void OnShareButtonClicked(object sender, EventArgs e)
+        {
+            ViewModel = Resources["vm"] as ArticleViewModel;
+            string articleShareLink = ViewModel.Article.ArticleUrl;
+            ShareUri(articleShareLink);
+        }
+
         async void OnDismissButtonClicked(object sender, EventArgs args)
         {
             await Navigation.PopModalAsync();
+        }
+
+        public async Task ShareUri(string uri)
+        {
+            await Share.RequestAsync(new ShareTextRequest
+            {
+                Uri = uri,
+                Title = "Share Article"
+            });
         }
     }
 }
